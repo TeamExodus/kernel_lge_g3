2a09a09f741e49c385f8fa4d2850839f3ef4509c
Adrian Grimm <usmcamgrimm@gmail.com>
2015-05-11 11:21:43 -0400

    Revert "Add Hyper gov"
    This reverts commit c1fa1d49cdc5cd9ccd400b5546a35f24bc3c215e.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -381,8 +381,6 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DANCEDANCE)
 extern struct cpufreq_governor cpufreq_gov_dancedance;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_DANCEDANCE)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
-extern struct cpufreq_governor cpufreq_gov_hyper;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
 extern struct cpufreq_governor cpufreq_gov_lionheart;
 #define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_LIONHEART)
