a41156fda9fca022cc732ba3086c1c8d2619398b
Adrian Grimm <usmcamgrimm@gmail.com>
2015-08-07 15:56:00 -0400

    Revert "misc: fix warnings when building with -O3"
    This reverts commit 5a7532e176fc86255f2064e82e8b8c207f235457.
    
    Revert "Simple GPU Algorithm: Initial coding for devfreq based Adreno Drivers"
    
    This reverts commit 9dbeb4267b0b5a4ae43ae28bea1317e43b6b74de.
    
    Revert "g3: enable O3 optimizations"
    
    This reverts commit 9e0403dedb4dac5d1b69a55bd0ba6ecd60f2f3d5.
    
    Change-Id: Ic5c260f77c2ca0429a46231dcf081616f51db41d

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -465,7 +465,7 @@ static int __cpufreq_set_policy(struct cpufreq_policy *data,
 static ssize_t store_##file_name					\
 (struct cpufreq_policy *policy, const char *buf, size_t count)		\
 {									\
-	unsigned int ret = 0;						\
+	unsigned int ret;						\
 	struct cpufreq_policy new_policy;				\
 									\
 	ret = cpufreq_get_policy(&new_policy, policy->cpu);		\
